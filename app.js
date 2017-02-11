682419b9855c1082f52a0a14a1216110fc7832d5
Graffiti <graffiti4u@naver.com>
2017-02-11 18:57:09 +0900

    Express, 정적파일을 서비스하는 법
    이미지나 텍스트 파일과 같은 정적인 파일을 서비스하는 방법.
    http://localhost:3000/route.png 로 정적인 파일을 바로 불러보기.
    http://localhost:3000/test.txt 로 정적인 파일을 바로 불러보기.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){  //URL로 직접 주소를 적어 서버에 접속할 때는 get메서드가 호출됨.
   res.send('Hello World!');
 });
-
 app.get('/login', function(req, res){
   res.send('<h1>Login please.</h1>');
+});
+app.get('/route', function(req, res){
+  res.send('Hello Router, <img src="/route.png">');
 });
 
 app.listen(3000, function(){
