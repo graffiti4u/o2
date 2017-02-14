d833a40350c903100e57b131582849dd30daffbf
Graffiti <graffiti4u@naver.com>
2017-02-14 16:31:14 +0900

    Express, 템플릿 엔진 3 : Jade의 문법
    jade express code pretty 검색으로 html 코드의 beautify 작업하기.
    jade 구문 내부에 자바스크립트를 사용할 시 - 를 붙여준다.
    jade 내부에 express가 보내주는 변수를 받아 사용하기 위해선 태그= 을 사용한다.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.locals.pretty = true;
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.use(express.static('public'));
 
 app.get('/template', function(req, res){
-  res.render('temp');
+  res.render('temp', {time:Date(), _title:'Jade'});
 });
 app.get('/', function(req, res){  //URL로 직접 주소를 적어 서버에 접속할 때는 get메서드가 호출됨.
   res.send('Hello World!');
