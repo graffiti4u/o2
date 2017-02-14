d2af102e9ae75ae1f2be833d41e2e310d59dd66f
Graffiti <graffiti4u@naver.com>
2017-02-14 15:37:20 +0900

    Express, 웹페이지를 표현하는 방법
    정적인 파일을 전달하는 방법과 동적인 방법을 전달하는 방법.
    ``: 그레이브액센트를 사용한 텍스트 코딩 방법.
    ${}를 사용하여 변수를 텍스트 내부에 전달하는 방법.

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ app.use(express.static('public'));
 app.get('/', function(req, res){  //URL로 직접 주소를 적어 서버에 접속할 때는 get메서드가 호출됨.
   res.send('Hello World!');
 });
+app.get('/dynamic', function(req, res){
+  var lis = '';
+  for(var i=0; i<5; i++){
+    lis += '<li>coding</li>';
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>static.html</title>
+    </head>
+    <body>
+      Hello, Dynamic!
+      <ul>
+      ${lis}
+      </ul>
+      ${time}
+    </body>
+  </html>
+  `;
+  res.send(output);
+});
 app.get('/login', function(req, res){
   res.send('<h1>Login please.</h1>');
 });
