a88dc93cb1563950c289351aa0e58774059c8c97
Graffiti <graffiti4u@naver.com>
2017-02-18 02:29:14 +0900

    웹앱 제작 6 : 코드의 개선
    readdir() 함수의 중복성을 없애자.
    서로 다른 라우트들의 중복성을 없애기위해 라우트를 배열로 처리할 수 있다.
    app.get(['/topic', '/topic/:id'], function(){});
    목록 뷰에서 글쓰기 기능을 주기위해 jade 파일 수정.
    new.jade 파일도 리스트를 보이게 만들어준다.(페이지들 간의 통일성을 맞추기 위해)

--- app_file.js
+++ app_file.js
@@ -10,27 +10,46 @@ app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/topic/new', function(req, res){
-  res.render('new');
-});
-app.get('/topic', function(req, res){
+  // new 뷰에도 목록 리스트를 뿌리기 위해. readdir()를 불러 옴.
   fs.readdir('data', function(err, files){
     if(err){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.render('view', {topics: files});
+    res.render('new', {topics:files});
   });
 });
+app.get(['/topic', '/topic/:id'], function(req, res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.id;
+    if(id){
+      // id 값이 있을 때
+      fs.readFile('data/' + id, 'utf8', function(err, data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics:files, title:id, description:data});
+      });
+    } else {
+      // id 값이 없을 때
+      res.render('view', {topics:files, title:'Welcome', description:'Hello, JavaScript for server'});
+    }
+  });
+});
+/*
 app.get('/topic/:id', function(req, res){
   var id = req.params.id;
   fs.readdir('data', function(err, files){
-  // 목록을 일단 위에 뿌려주기 위해 readdir()함수를 호출
     if(err){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
     fs.readFile('data/' + id, 'utf8', function(err, data){
-    // 해당 파일의 실제 내용을 읽어들이기 위해 readFile() 호출
       if(err){
         console.log(err);
         res.status(500).send('Internal Server Error');
@@ -39,6 +58,7 @@ app.get('/topic/:id', function(req, res){
     });
   });
 });
+*/
 app.post('/topic', function(req, res){
   var title = req.body.title;
   var description = req.body.description;
@@ -47,7 +67,8 @@ app.post('/topic', function(req, res){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.send('Success!');
+    res.redirect('/topic/' + title);
+    // new에 의해 파일을 만든 후 화면을 해당 만든 파일의 라우트를 보여주자.
   });
 });
 
