c4f38d515875e13a51686e0c90a71e4b54d27b84
Graffiti <graffiti4u@naver.com>
2017-02-17 22:19:28 +0900

    웹앱 제작 3 : 본문 저장
    form태그에 의해 전달되어지는 내용을 처리하기 위해 body-parser모듈을 설치.
    data 폴더에 파일로 데이터를 관리하기 위해 내장 fs 모듈 활용.

--- app_file.js
+++ app_file.js
@@ -1,15 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();  //express 모듈에 있는 express함수를 호출하면 웹서버 객체가 생성된다.
 
 app.locals.pretty = true; //html 소스보기 했을 때 jade로 만들어진 파일을 beautify하게 만들어 줌.
 app.set('views', './views_file');
 app.set('view engine', 'jade');
 
+app.use(bodyParser.urlencoded({ extended: false}));
+// form에 의해 전달되어지는 문자열에는 공백(한칸뛰기)이 있게 되는데 그부분을 "+"기호로 변환할지를 물음.
+// 또한 특수문자는 ASCII 16진수 값으로 전환할 것인지를 물음.
+// req.body.속성이름으로 해당 데이터에 접근할 수 있게 만들어 줌.
+
 app.get('/topic/new', function(req, res){
   res.render('new');
 });
 app.post('/topic', function(req, res){
-  res.send('Hi, POST!');
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      console.log(err); // 정확한 에러단서를 찾기위해 로그를 확인해 보자.
+      res.status(500).send('Internal Server Error');
+    }
+    res.send('Success!');
+  });
 });
 
 app.listen(3000, function(){
