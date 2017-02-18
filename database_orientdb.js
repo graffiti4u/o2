95c9216ac07a408bf781547eb630a885f9c55bde
Graffiti <graffiti4u@naver.com>
2017-02-18 23:13:18 +0900

    Orientdb로 웹애플리케이션 제작 6 : INSERT & UPDATE & DELETE
    CRUD의 기본을 테스트 해 보기.

--- database_orientdb.js
+++ database_orientdb.js
@@ -21,6 +21,7 @@ db.query(sql).then(function(results){
   console.log(results); //결과물은 배열을 리턴해 준다.
 });
 */
+/* 2.
 var sql = 'SELECT FROM topic WHERE @rid=:rid';
 var param = {
   params:{  //params는 약속되어진 문법이기 때문에 그대로 사용할 것.
@@ -30,4 +31,37 @@ var param = {
 db.query(sql, param).then(function(results){
   console.log(results); //결과물은 배열을 리턴해 준다.
 });
+*/
 // query()함수에는 원하는 sql을 받을 수 있고 또한 추가적으로 가변변수(:rid)를 sql에 작성하여 변수값을 함께 가져올 수도 있다.
+
+// INSERT
+/*
+var sql = "INSERT INTO topic (title, description) VALUES(:title, :desc)";
+db.query(sql, {
+  params:  {
+    title:'Express',
+    desc:'Express is framework for web.'
+  }
+}).then(function(results){
+  console.log(results);
+});
+*/
+
+// UPDATE
+/*
+var sql = "UPDATE topic SET title=:title WHERE @rid=:rid";
+db.query(sql, {
+  params:{title:'Expressjs', rid:'#33:1'}
+}).then(function(results){
+  console.log(results);
+});
+// 수정행위를 하면 몇개의 결과가 수정되는지 숫자로 보여준다.
+*/
+
+// DELETE
+var sql = "DELETE FROM topic WHERE @rid=:rid";
+db.query(sql, {
+  params:{rid:'#34:0'}
+}).then(function(results){
+  console.log(results);
+});
