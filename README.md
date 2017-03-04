b61c201293db1ba63b2e63083b897862710a7bd2
Graffiti <graffiti4u@naver.com>
2017-03-05 02:29:26 +0900

    cookie 6 : cookie & security
    쿠키를 암호화 하여 req정보와 res정보의 사용하는 방법을 알자.

--- README.md
+++ README.md
@@ -79,3 +79,4 @@
 077. cookie 3 : shopping cart 1
 078. cookie 4 : shopping cart 2
 079. cookie 5 : shopping cart 3
+080. cookie 6 : cookie & security
--- app_cookie.js
+++ app_cookie.js
@@ -2,7 +2,8 @@ var express = require('express');
 var cookieParser = require('cookie-parser'); //쿠키를 사용하기 위해 쿠키 모듈 설치.
 var app = express();
 
-app.use(cookieParser());  //쿠키를 사용하기 위해 cookieParser 미들웨어를 설치해 준다.
+app.use(cookieParser('ief3kdkf0-04u5lj32rdkgjfgjb'));  // 쿠키를 사용하기 위해 cookieParser 미들웨어를 설치해 준다.
+// 1. 쿠키를 암호화 하기 위해 키값을 랜덤하게 셋팅해 준다.
 
 // 데이터베이스 대용으로 쿠키로 보내고자 하는 데이터를 객체 샘플로 생성.
 var products = {
@@ -33,8 +34,8 @@ cart = {
 */
 app.get('/cart/:id', function(req, res){
   var id = req.params.id;
-  if(req.cookies.cart){
-    var cart = req.cookies.cart;
+  if(req.signedCookies.cart){
+    var cart = req.signedCookies.cart;
   } else {
     var cart = {};
   }
@@ -42,12 +43,12 @@ app.get('/cart/:id', function(req, res){
     cart[id] = 0;
   }
   cart[id] = parseInt(cart[id]) + 1;
-  res.cookie('cart', cart);
+  res.cookie('cart', cart, {signed: true});
   console.log(cart);
   res.redirect('/cart');
 });
 app.get('/cart', function(req, res){
-  var cart = req.cookies.cart;  //사용자가 보낸 cart에 대한 쿠키값을 받는다.
+  var cart = req.signedCookies.cart;  //사용자가 보낸 cart에 대한 쿠키값을 받는다.
   if(!cart){
     res.send('Empty!');
   } else {
@@ -65,14 +66,14 @@ app.get('/cart', function(req, res){
 
 app.get('/count', function(req, res){
   var count = 0;
-  if(req.cookies.count){
-     count = parseInt(req.cookies.count);  //쿠기값은 문자로 전송되기 때문에 계산을 위해서는 숫자로 전환.
+  if(req.signedCookies.count){  // 2. 브라우져가 요청할 때 넘어온 쿠키를 암호화 한다.
+     count = parseInt(req.signedCookies.count);  //쿠기값은 문자로 전송되기 때문에 계산을 위해서는 숫자로 전환.
   } else {
      count = 0;
   }
   count += 1;
   // count라우트로 접속했을시 응답할 때 쿠키의 값을 셋팅하고 브라우져에게 보내자.
-  res.cookie('count', count);
+  res.cookie('count', count, {signed: true}); // 쿠키를 구워서 클라이언트에게 보낼 때도 암호화해서 보낸다.
   res.send('count : ' + count); //웹브라우저가 웹서버에게 전송(요청)한 데이터를 확인 할 수 있다.
   //쿠키는 동일한 주소에서 셋팅 되었을 때 데이터 상태가 유지된다. 다른 주소로의 접속 시 쿠키는 인식을 하지 않는다.
 
